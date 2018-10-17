7de8c7fe73af8c7093280f63983fc56f6860585b
Jaimemosg <jmosquerag@unal.edu.co>
2018-10-17 11:29:38 -0500

    Función programada, con dos ejemplos incorporados.

--- R/SumBoth.R
+++ R/SumBoth.R
@@ -1,3 +1,30 @@
 #' SumBoth
 #' 
-#' This function takes two real numbers and returns their summation.
+#' @author Jaime Mosquera Gutiérrez, \email{jmosquerag@unal.edu.co}
+#' 
+#' 
+#' This function takes two real numbers and returns their summation
+#' 
+#' 
+#' @examples
+#' 
+#' @importFrom stats
+#' @export
+#' # Sum two known numbers
+#' x <- 1
+#' y <- 2
+#' # x + y = 1 + 2 = 3
+#' z <- SumBoth(x, y)
+#' z
+#' 
+#' # What happens if input is not from numeric class
+#' x <- "a"
+#' y <- 2
+#' SumBoth(x, y)
+#'  
+#' SumBoth function ---------------------------------------------------------------------
+SumBoth <- function(num1, num2) {
+  if(class(num1) !=  "numeric") return("num1 must be numeric")
+  if(class(num2) !=  "numeric") return("num2 must be numeric")
+  if(class(num2) ==  "numeric" & class(num2) == "numeric") return(num1 + num2)
+}
